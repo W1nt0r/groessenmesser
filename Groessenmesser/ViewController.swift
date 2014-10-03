b0147c297369ed6187687d3691b017c1b78a8931
W1nt0r <j4n.winter@gmail.com>
2014-10-03 15:22:21 +0200

    Initial Commit

---
+++ Groessenmesser/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Groessenmesser
+//
+//  Created by Jan Winter on 03.10.14.
+//  Copyright (c) 2014 HSR. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
